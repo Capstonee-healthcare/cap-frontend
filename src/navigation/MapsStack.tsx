25b9f9609aa577a4fb821be758738398ca6472d2
souravshetty <sourav.s.shetty@gmail.com>
2025-06-27 14:33:01 -0700

    changes for vision camera gradle

--- src/navigation/MapsStack.tsx
+++ src/navigation/MapsStack.tsx
@@ -1,7 +1,7 @@
 // src/navigation/MapsStack.tsx
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import MapsScreen from "../screens/chatfeature/MapsScreen";
+import MapsScreen from "../screens/Maps/MapsScreen";
 import ClinicDetailsScreen from "../screens/chatfeature/ClinicDetailsScreen";
 
 const Stack = createNativeStackNavigator();
